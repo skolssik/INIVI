ff078f32b9c465d406020bc14bf4f4f6daa7fb3f
Antony <skolssik@gmail.com>
2023-12-16 15:16:39 +0300

    form

--- js/form.js
+++ js/form.js
@@ -48,3 +48,24 @@ document.addEventListener('DOMContentLoaded', function () {
     return /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i.test(input.value);
   }
 });
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Форма успешно отправлена");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
